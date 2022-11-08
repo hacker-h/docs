2c8864ce325b68cb7b1f1eca706c76ae0940d753
Henning Häcker <henning.haecker+github.com@protonmail.com>
2022-11-08 01:07:57 +0100

    minor fixes

--- flashing/sonoff-zigbee-dongle/setup.sh
+++ flashing/sonoff-zigbee-dongle/setup.sh
@@ -12,13 +12,15 @@ source ${VIRTUALENV_PATH}/bin/activate
 # Install dependencies
 pip3 install --upgrade pyserial intelhex gevent
 
+mkdir -p ${DEPENDENCIES_PATH}
+
 # Download Bootloader Activation tool
 VERSION="1.0"
 CHECKSUM="d9af1116ff38cf32ee5c5f73a817a9bb3783e7cc74ba60d716811ddee5f2ff96"
 until echo "${CHECKSUM} ${DEPENDENCIES_PATH}/uartLog.py" | sha256sum -c;
 do
 wget -O ${DEPENDENCIES_PATH}/uartLog.py \
-    https://raw.githubusercontent.com/hacker-h/Sonoff_zigbee_3.0_USB_dongle_plus_bootloader_script/1.0/uartLog.py;
+    https://raw.githubusercontent.com/hacker-h/Sonoff_zigbee_3.0_USB_dongle_plus_bootloader_script/${VERSION}/uartLog.py;
 done
 
 # Download Flashing tool
@@ -27,6 +29,6 @@ CHECKSUM="85b4377f65e502ca5117853c06462c0af3dc181bf103ba3fbe4074cb6f7ba6bf"
 until echo "${CHECKSUM} ${DEPENDENCIES_PATH}/cc2538-bsl.py" | sha256sum -c;
 do
 wget -O ${DEPENDENCIES_PATH}/cc2538-bsl.py \
-    https://raw.githubusercontent.com/hacker-h/cc2538-bsl/v1.0/cc2538-bsl.py;
+    https://raw.githubusercontent.com/hacker-h/cc2538-bsl/${VERSION}/cc2538-bsl.py;
 done
 
